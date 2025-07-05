5c798890a6681dbd787500c92d09ac7b0bdaf9b9
MPSxDev <mparedes.digital@gmail.com>
2025-07-04 23:07:45 -0400

    Update express type definitions

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,26 @@
 import { User } from "../entities/User";
+import { UserRole } from "../enums/UserRole";
+import { Merchant } from "../interfaces/webhook.interfaces";
 
 declare module "express-serve-static-core" {
   interface Request {
-    user?: Partial<User>;
+    user?: {
+      id: number;
+      email: string;
+      name?: string;
+      role?: UserRole;
+      tokenExp?: number;
+      jti?: string;
+      isEmailVerified?: boolean;
+      isWalletVerified?: boolean;
+      walletAddress?: string;
+      logoUrl?: string;
+      createdAt?: Date;
+      updatedAt?: Date;
+    };
+    merchant?: Merchant;
     validatedIp?: string;
     tokenExp?: number;
+    requestId?: string;
   }
 }
