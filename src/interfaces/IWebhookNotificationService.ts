7abf9c6d766f0e9f3d53774640f93a6e5ecc283c
DioChuks <diochuks65@gmail.com>
2025-09-30 21:02:22 +0100

    fix: update payload type in sendWebhookNotification method to WebhookPayload

--- src/interfaces/IWebhookNotificationService.ts
+++ src/interfaces/IWebhookNotificationService.ts
@@ -1,7 +1,14 @@
+import { WebhookPayload, MerchantWebhook } from "./webhook.interfaces";
+
 export interface IWebhookNotificationService {
   sendWebhookNotification(
     url: string,
-    payload: any,
+    payload: WebhookPayload,
     merchantId: string
+  ): Promise<boolean>;
+
+  notifyPaymentUpdate(
+    merchantWebhook: MerchantWebhook,
+    paymentDetails: Omit<WebhookPayload, "timestamp">
   ): Promise<boolean>;
 }
